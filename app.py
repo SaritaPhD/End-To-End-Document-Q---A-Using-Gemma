a7d83af45f6aa06ee108b22723e929523bda97c3
Sarita <126271555+SaritaPhD@users.noreply.github.com>
2024-12-01 19:11:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st 
-import os
+import os 
 from langchain_groq import ChatGroq 
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains.combine_documents import create_stuff_documents_chain
